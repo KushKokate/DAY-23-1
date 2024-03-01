1ccb41903bf7d5331269660791a0171127f42f2f
Kush <kokatekush@gmail.com>
2024-03-01 11:52:59 +0530

    Initial commit

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,15 @@
+PLACEHOLDER = "[name]"
+with open("./Input/Names/invited_names.txt", mode= 'r') as names_file:
+    # redlines() method
+    names = names_file.readlines()
+with open("./Input/Letters/starting_letter.txt", mode= 'r') as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        #strip method
+        stripped_name = name.strip()
+        #replace method
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/Letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
+
